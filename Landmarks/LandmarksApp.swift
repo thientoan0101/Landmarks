3a07f66e8a2b3a4aba2605af0265801fc9f0b1db
Nguyen Tran Thien Toan <thientoancubi@gmail.com>
2022-04-10 10:39:42 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Toan Thien on 10/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
