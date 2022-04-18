eba55499c905edc5771f714a10a24e2559c2d29b
Nguyen Tran Thien Toan <thientoancubi@gmail.com>
2022-04-18 10:45:22 +0700

    add badge icon

---
+++ Hike.swift
@@ -0,0 +1,35 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by Toan Thien on 18/04/2022.
+//
+
+import Foundation
+
+struct Hike: Codable, Hashable, Identifiable {
+    
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: Int
+    var observation: [Observation]
+    
+    static var formatter = LengthFormatter()
+    
+    var distanceText: String {
+        Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
+    }
+
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+    
+    
+    
+}
---
+++ Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Toan Thien on 18/04/2022.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
